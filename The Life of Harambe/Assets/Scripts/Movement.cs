090f7da6ce9a3f4f2d692bca50268630a7ed3216
joshuadjacoby <joshuadjacoby@gmail.com>
2016-08-18 01:59:30 -0700

    pushed already done work, current progress: working game where player can move side to side, bananas and kids will fall from the top, no score yet.

---
+++ The Life of Harambe/Assets/Scripts/Movement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour
+{
+
+    private bool moveLeft;
+    private bool moveRight;
+
+    Vector2 forwardSpeed = new Vector2(5, 0);
+
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Harambe");
+    }
+
+    void Update()
+    {
+        if (moveLeft && !moveRight)
+        {
+            if (!(player.transform.position.x < -2))
+                player.GetComponent<Rigidbody2D>().velocity = -forwardSpeed;
+            else
+                StopMeLeft();
+        }
+        if (moveRight && !moveLeft)
+        {
+            if (!(player.transform.position.x > 2))
+                player.GetComponent<Rigidbody2D>().velocity = forwardSpeed;
+            else
+                StopMeRight();
+        }
+    }
+
+    public void MoveMeLeft()
+    {
+        moveLeft = true;
+    }
+
+    public void StopMeLeft()
+    {
+        moveLeft = false;
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+
+    public void MoveMeRight()
+    {
+        moveRight = true;
+    }
+
+    public void StopMeRight()
+    {
+        moveRight = false;
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+}
---
+++ The Life of Harambe/Assets/Scripts/Spawn_Stuff.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn_Stuff : MonoBehaviour
+{
+
+    public GameObject banana;
+
+    Vector3 spawn_position;
+    public double timer = 0.0;
+
+    public GameObject[] kids = new GameObject[5];
+
+    void spawn_item()
+    {
+        int[] pos = new int[] { -2, 0, 2 };
+        spawn_position = new Vector3(pos[Random.Range(0, 3)], 2.6f, 0);
+        if (Random.value > 0.7)
+        {
+            GameObject temp_spawn_kid = Instantiate(kids[Random.Range(0, 5)], spawn_position, Quaternion.identity) as GameObject;
+        }
+        else
+        {
+            GameObject temp_spawn_banana = Instantiate(banana, spawn_position, Quaternion.identity) as GameObject;
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 2)
+        {
+            spawn_item();
+            timer = 0.0;
+        }
+    }
+}
