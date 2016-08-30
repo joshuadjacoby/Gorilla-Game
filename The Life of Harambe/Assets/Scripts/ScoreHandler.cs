80dc84efecfaca24bd4b60d69c23208df91bab1c
joshuadjacoby <joshuadjacoby@gmail.com>
2016-08-30 12:49:29 -0700

    Added missing assets

---
+++ The Life of Harambe/Assets/Scripts/ScoreHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+//Keeps track of score value throughout all scenes
+public class ScoreHandler : MonoBehaviour {
+    public static int score;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void Awake ()
+    {
+        score = 0;
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
