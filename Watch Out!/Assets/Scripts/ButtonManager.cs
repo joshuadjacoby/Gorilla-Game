50fe4ac25543c8057beb88319cd1a338faea7595
joshuadjacoby <joshuadjacoby@gmail.com>
2016-09-01 11:41:54 -0700

    Accelerometer movement no longer jitters, need to refine speed

---
+++ Watch Out!/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour {
+    GameObject pauseButton;
+
+    Sprite play;
+    Sprite pause;
+
+	// Use this for initialization
+	void Start ()
+    {
+        pauseButton = GameObject.Find("Pause Button");
+
+        play = Resources.Load<Sprite>("Play");
+        pause = Resources.Load<Sprite>("Pause");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void startGame () 
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+	public void pauseGame () 
+	{
+        if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            pauseButton.GetComponent<Image>().sprite = pause;
+        }
+        else
+        {
+            Time.timeScale = 0;
+            pauseButton.GetComponent<Image>().sprite = play;
+        }
+	}
+
+    public void newGame ()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
---
+++ Watch Out!/Assets/Scripts/Movement.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour
+{
+
+    private bool moveLeft;
+    private bool moveRight;
+
+	Vector2 forwardSpeed;
+
+    GameObject player;
+	
+    Vector3 screen;
+
+    void Start()
+    {
+		forwardSpeed = new Vector2(5, 0);
+
+        player = GameObject.Find("Gorilla");
+
+        screen = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+    }
+
+    void Update()
+    {
+        getDirection();
+        if (moveLeft && !moveRight)
+        {
+            if (!(player.transform.position.x < -screen.x * .74))
+                player.transform.Translate(Mathf.Max(Input.acceleration.x, -.15f), 0, 0);
+            //player.GetComponent<Rigidbody2D>().velocity = -forwardSpeed;
+            else
+                StopMeLeft();
+        }
+		if (moveRight && !moveLeft)
+        {
+            if (!(player.transform.position.x > screen.x * .74))
+                player.transform.Translate(Mathf.Min(Input.acceleration.x, .15f), 0, 0);
+            //player.GetComponent<Rigidbody2D>().velocity = forwardSpeed;
+            else
+                StopMeRight();
+        }
+    }
+    
+    //Determines the players x direction
+    void getDirection()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.acceleration.x < 0)
+            MoveMeLeft();
+        else
+            StopMeLeft();
+        if (Input.GetKey(KeyCode.RightArrow) || Input.acceleration.x > 0)
+            MoveMeRight();
+        else
+            StopMeRight();
+        if(Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow))
+            player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+
+    void MoveMeLeft()
+    {
+        moveLeft = true;
+    }
+
+    void StopMeLeft()
+    {
+        moveLeft = false;
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+
+    void MoveMeRight()
+    {
+        moveRight = true;
+    }
+
+    void StopMeRight()
+    {
+        moveRight = false;
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+}
