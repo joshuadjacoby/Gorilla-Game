9f87c5e081a5b5e5807db9e45184463992f62d84
joshuadjacoby <joshuadjacoby@gmail.com>
2016-09-05 00:42:34 -0700

    Finalized game, ready for release. Added sounds, game over scene is done, everything is ready.

--- Watch Out!/Assets/Scripts/SpawnStuff.cs
+++ Watch Out!/Assets/Scripts/SpawnStuff.cs
@@ -36,7 +36,7 @@ public class SpawnStuff : MonoBehaviour
         };
 
         counter = 0;
-        spawn_time = 2.1;
+        spawn_time = 2;
         spawn_freq = .8;
 
         screen = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
